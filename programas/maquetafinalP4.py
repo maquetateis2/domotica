4754437700da2d8993e990d5c48391a3c1cd6b1f
saraap683 <saraaguado@iesteis.net>
2025-05-05 09:23:14 +0200

    Update maquetafinalP4.py

--- programas/maquetafinalP4.py
+++ programas/maquetafinalP4.py
@@ -1,12 +1,12 @@
  from microbit import*
  
  servo1 = pin2
- angulos = 0
+ angulos = 1
  
  while True :
      if butto_b.is_pressed():
          angulo +=10
          servo.write_analog(angulo)
          if angulo >= 90:
-             angulo = 0
+             angulo = 1
              servo write_analog(angulo)
