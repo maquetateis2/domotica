fe07eb1b4afd3c815ebc1bfaf8e47f5e389c01e1
saraap683 <saraaguado@iesteis.net>
2025-05-06 14:02:40 +0200

    Update P4.py

--- programas/P4.py
+++ programas/P4.py
@@ -3,15 +3,16 @@ Autores: Sara Aguado Pedrido
 Data: 30/04/2025"""
 
 from microbit import*
- pin2_set_analog_period(20) # Declaramos que e pin 2 es analógico
- pin2.writte_analog(1) # Servo a 1º para comenzar coa porta pechada
- 
+ pin2.set_analog_period(20) # Declaramos que e pin 2 es analógico
+ pin2.write_analog(1) # Servo a 1º para comenzar coa porta pechada
+ pin2.write_analog(90)
  angulos = 0
  
  while True :
-     if butto_b.is_pressed():
+     if button_b.is_pressed():
          angulo +=10
          servo.write_analog(angulos)
-         if angulo >= 90:
-             angulo = 1
-             servo write_analog(angulos)
+     if angulo >= 90:
+        angulo = 1
+        servo write_analog(angulos)
+sleep(1000)
