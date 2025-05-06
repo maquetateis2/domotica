ab1ae9d40b67b7d5bd974f0eb1a211c818dd43ea
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-06 14:16:33 +0200

    Update P2Sasha.py

--- programas/P2Sasha.py
+++ programas/P2Sasha.py
@@ -6,13 +6,13 @@ from microbit import * # importamos libreria
 
 led = pin14 # conectamos a pin
 
-while True: # para siempre
-    luz = pin1.read_analog()
+while True: # facemos que trabaja siempre
+    luz = pin1.read_analog() # leemos el balor
 
-    if luz < 700:
-        led.write_digital(1)
+    if luz < 700:            # si luz < que 700 es noche 
+        led.write_digital(1) # y Led encendemos
 
-    else:
-        led.write_digital(0)
+    else:                    # en otro caso 
+        led.write_digital(0) # lo apagamos
 
     sleep(1000)
