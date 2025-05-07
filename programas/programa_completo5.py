20a278a7ebfd4653aa34367aacc0334e79afdbce
Carla Tresandí <tresandic@gmail.com>
2025-05-07 14:13:51 +0200

    Update programa_completo5.py

--- programas/programa_completo5.py
+++ programas/programa_completo5.py
@@ -5,27 +5,27 @@ Data: 05/05/2025"""
 from microbit import *
 import neopixel
 
-np = neopixel.neoPixel(pin13, 1)
-led_blanco = pin14
-sensor_pir = pin15
+np = neopixel.neoPixel(pin13, 1)    # Crea unha tira de 1 LED NeoPixel conectado ao pin 13
+led = pin14                         # Define o pin 14 como "led"
+sensor = pin15                      # Define o pin 15 como "sensor"
 
-    for _ in range(5):
-        np[0] = (255, 0, 0)
-        np.show()
-        led_blanco.write_digital(1)
-        display.show(Image.ANGRY)
-        sleep(500)
-        np[0] = (0, 0, 0)
-        np.show()
-        led_blanco.write_digital(0)
-        display.clear()
+    for _ in range(5):               # Repite 5 veceso seguinte bloque
+        np[0] = (255, 0, 0)          # Acende o LED NeoPixel en vermello
+        np.show()                    # Mostra o cambio do color no LED
+        led.write_digital(1)         # Acende o LED branco conectadoao pin 14
+        display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit    
+        sleep(500)                   # Espera 500 milisegundos
+        np[0] = (0, 0, 0)            # Apaga o LED NeoPixel
+        np.show()                    # Actualiza o estado do LED
+        led.write_digital(0)         # Apaga o LED branco
+        display.clear()              # Borra o que se mostra na pantalla
         sleep(500)
         
-while True:
-    if sensor_pir.read_digital() == 1:
+while True:     # Bucle infinito
+    if sensor.read_digital() == 1:    # Se o sensor PÎR detecta movemento
+     music.play(music.RINGTONE)       # Reproduce un ton de llamada
+     sleep(500)     
      music.play(music.RINGTONE)
-     sleep(500)
-     music.play(music.RINGTONE)()
-    else:
-        display.show(Image.HOUSE)
-    sleep(100)
+    else:                             # Se non detecta movemento
+        display.show(Image.HOUSE)     # Mostra unha imaxe dunha casa na pantalla
+    sleep(100) 
