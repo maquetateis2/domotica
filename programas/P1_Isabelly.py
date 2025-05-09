c4e8e9885dd9c8ce0adc11d82f86e9e397594927
Isabelly-s <isabellydasilva@iesteis.net>
2025-05-09 14:18:31 +0200

    Update P1_Isabelly.py

--- programas/P1_Isabelly.py
+++ programas/P1_Isabelly.py
@@ -5,38 +5,22 @@ Data:05/05/2025
 from microbit import *
 import neopixel
 
-temp = temperature()
-np = neopixel.NeoPixel(pin13,2)
+np = neopixel.NeoPixel(pin13, 2)  
+
+led = pin14 
 np.clear()
 while True:
-   if temp> 24:
-     np[0]= (0, 255, 0)
-     np[1]= (0, 255, 0)
-     np.show()
-     pin16.write_digital(1)
+    temp = temperature()
 
+    if temp > 20:
+        np[0] = (0, 255, 0)  
+        np[1] = (0, 255, 0)
+        np.show()  
+        led.write_digital(1)  
+    else:
+        np[0] = (255, 0, 0)  
+        np[1] = (255, 0, 0) 
+        np.show()  
+        led.write_digital(0)  
 
-   elif 20<=temp:
-     np[0]= (0, 0, 255)
-     np[1]= (0, 0, 255)
-     pin16.write_digital(0)
-     np.clear()
-
-   elif temp>20 and temp<=22:
-     np[0]= (255, 0, 0)
-     np[1]= (255, 0, 0)
-     pin16.write_digital(0)
-     np.clear()
-
-   elif temp>22 and temp<=24:
-     np[0]= (255, 165, 0)
-     np[1]= (255, 165, 0)
-     pin16.write_digital(0)
-     np.clear()
-   else:
-     np[0]= (0, 0, 0)
-     np[1]= (0, 0, 0)
-     pin16.write_digital(0)
-     np.clear()
-
-sleep(1000)
+    sleep(1000)  
