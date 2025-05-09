bc8f1b7693dae3f12696e938a5b12b1c9517dcd1
Carla Tresandí <tresandic@gmail.com>
2025-05-09 13:58:48 +0200

    Update maqueta_final_carla.py

--- programas/maqueta_final_carla.py
+++ programas/maqueta_final_carla.py
@@ -5,12 +5,12 @@ Data: 06/05/2025"""
 from microbit import *
 import neopixel
 
-np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
+np = neopixel,NeoPixel(pin 13, 2)  # 2 LED neopixel conectados ao pin 13
 
-led = pin14 # LED branco conectado ao pin14
+led = pin 14 # LED branco conectado ao pin14
 np.clear()
 while True:
-    temperatura = temperature() # gardamos valor da temperatura
+    temperatura = temperature(1) # gardamos valor da temperatura
 
     if temperatura > 20:
         np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
