271bea0459d5f96f77cb2cbbbe01e85cc60e04c3
saraap683 <saraaguado@iesteis.net>
2025-05-09 14:24:20 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -68,9 +68,22 @@ servo1 = pin2
              angulo = 0
              servo write_analog(angulo)
 
-from microbit import *
-import neopixel
-import music
+
+pin2.set_analog_period(20) # Servo coenctado al pin 2
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
+ 
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
+
+    sleep(100)
+
 
 np = neopixel.NeoPixel(pin13, 2)    # Crea unha tira de 1 LED NeoPixel conectado ao pin 13
 led = pin14                         # Define o pin 14 como "led"
