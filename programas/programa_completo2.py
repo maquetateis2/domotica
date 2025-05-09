34def124e13dfad6b6a68762aa74969acd738184
saraap683 <saraaguado@iesteis.net>
2025-05-09 14:10:31 +0200

    programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -1,4 +1,44 @@
 """Programa completo da fila 1 para a maqueta domótica. 
-Autores: 
-Data: 
+Autores: Isabelly, Sasha, Leyla, Sara.A, Carla.
+Data: 9/05/2025
 """
+
+
+from microbit import *
+import neopixel
+
+temp = temperature()
+np = neopixel.NeoPixel(pin13,2)
+np.clear()
+while True:
+   if temp> 24:
+     np[0]= (0, 255, 0)
+     np[1]= (0, 255, 0)
+     np.show()
+     pin16.write_digital(1)
+
+
+   elif 20<=temp:
+     np[0]= (0, 0, 255)
+     np[1]= (0, 0, 255)
+     pin16.write_digital(0)
+     np.clear()
+
+   elif temp>20 and temp<=22:
+     np[0]= (255, 0, 0)
+     np[1]= (255, 0, 0)
+     pin16.write_digital(0)
+     np.clear()
+
+   elif temp>22 and temp<=24:
+     np[0]= (255, 165, 0)
+     np[1]= (255, 165, 0)
+     pin16.write_digital(0)
+     np.clear()
+   else:
+     np[0]= (0, 0, 0)
+     np[1]= (0, 0, 0)
+     pin16.write_digital(0)
+     np.clear()
+
+sleep(1000)
