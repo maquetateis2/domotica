c3f5ef6b69b4ac9b493755361ab8d403779fa1d4
maquetateis2 <maqueta2@iesteis.net>
2025-05-11 10:57:32 +0200

    Update P1Bernardo.py

--- programas/P1Bernardo.py
+++ programas/P1Bernardo.py
@@ -1,27 +1,30 @@
-"""Programa que acende leds en vermello se a temperatura pasa de 20º
+"""P1 maqueta. Sensor de temperatura
 Autor: Bernardo Álvarez
-Data: 20/040/2025"""
+5/4/25"""
 
 from microbit import *
 import neopixel
 
 np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
-
-led = pin14 # LED branco conectado ao pin14
 np.clear()
-while True:
-    temperatura = temperature() # gardamos valor da temperatura
+rele = pin16  # relé conectado ao pin 16
 
-    if temperatura > 20:
+while True:
+    temperatura = temperature()  # gardamos valor da temperatura
+
+    if temperatura > 18:
         np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
         np[1] = (0, 255, 0)
         np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(1)  # Acender o LED normal
+        rele.write_digital(1)  # Acender o LED normal
     else:
         np[0] = (255, 0, 0)  # Apagar os Neopixel
         np[1] = (255, 0, 0)  # Acender os Neopixel en verde
         np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(0)  # Apagar o LED normal
+        rele.write_digital(0)  # Apagar o LED normal
 
     sleep(1000)  # Esperar 1 segundo
+
+
+
 
