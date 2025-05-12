94fe554420588151ce87ed7a17248c8439191ec0
Isabelly-s <isabellydasilva@iesteis.net>
2025-05-12 09:24:34 +0200

    Update P1_Isabelly.py

--- programas/P1_Isabelly.py
+++ programas/P1_Isabelly.py
@@ -7,7 +7,7 @@ import neopixel
 
 np = neopixel.NeoPixel(pin13, 2)  
 
-led = pin14 
+rele = pin16 
 np.clear()
 while True:
     temp = temperature()
@@ -16,11 +16,11 @@ while True:
         np[0] = (0, 255, 0)  
         np[1] = (0, 255, 0)
         np.show()  
-        led.write_digital(1)  
+        rele.write_digital(1)  
     else:
         np[0] = (255, 0, 0)  
         np[1] = (255, 0, 0) 
         np.show()  
-        led.write_digital(0)  
+        rele.write_digital(0)  
 
     sleep(1000)  
