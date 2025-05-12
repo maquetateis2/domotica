8ec3ae85067be5e9ebe4dfd762839424659271ae
Carla Tresandí <tresandic@gmail.com>
2025-05-12 09:44:49 +0200

    Update programa5_comprobado.py

--- programas/programa5_comprobado.py
+++ programas/programa5_comprobado.py
@@ -8,20 +8,21 @@ import music
 
 np = neopixel.NeoPixel(pin13, 2)    # Crea unha tira de 1 LED NeoPixel conectado ao pin 13
 led = pin14                         # Define o pin 14 como "led"
-sensor = pin15.read_digital()       # Define o pin 15 como "sensor"
+
 
 while True:                           # Bucle infinito
+    sensor = pin15.read_digital()       # Define o pin 15 como "sensor"
     if sensor == 1:                   # Se o sensor PÎR detecta movemento
         music.play(music.RINGTONE)       # Reproduce un ton de llamada
         sleep(500)     
         music.play(music.RINGTONE)
         
         for i in range(5):               # Repite 5 veceso seguinte bloque
+            display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit  
             np[0] = (0, 255, 0)          # Acende o LED NeoPixel en vermello
             np[1] = (0, 255, 0)
             np.show()                    # Mostra o cambio do color no LED
             led.write_digital(1)         # Acende o LED branco conectadoao pin 14
-            display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit    
             sleep(500)                   # Espera 500 milisegundos
             np[0] = (0, 0, 0)            # Apaga o LED NeoPixel
             np[1] = (0, 0, 0)
@@ -34,3 +35,4 @@ while True:                           # Bucle infinito
         display.show(Image.HOUSE)     # Mostra unha imaxe dunha casa na pantalla
     sleep(100) 
       
+  
