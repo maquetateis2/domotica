445edb490672ab2b87aee5816b51c2a6879cda0e
Carla Tresandí <tresandic@gmail.com>
2025-05-16 13:59:12 +0200

    Update Programa_final_carla.py

--- programas/Programa_final_carla.py
+++ programas/Programa_final_carla.py
@@ -5,33 +5,22 @@ Data: 06/05/2025"""
 from microbit import *
 import neopixel
 
-np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
-led = pin14     # LED branco conectado ao pin 14
-luz = pin1.read_analog()     # Sensor de luz conecrtado ao pin 1
+np = neopixel.NeoPixel(pin13, 2)  
+
+rele = pin16 
 np.clear()
-
 while True:
-    temperatura = temperature()    # gardamos valor da temperatura
+    temp = temperature()
 
-    if temperatura > 20:
-        np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
+    if temp > 20:
+        np[0] = (0, 255, 0)  
         np[1] = (0, 255, 0)
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(1)  # Acender o LED normal
+        np.show()  
+        rele.write_digital(1)  
     else:
-        np[0] = (255, 0, 0)  # Apagar os Neopixel
-        np[1] = (255, 0, 0)  # Acender os Neopixel en verde
-        np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(0)  # Apagar o LED normal
+        np[0] = (255, 0, 0)  
+        np[1] = (255, 0, 0) 
+        np.show()  
+        rele.write_digital(0)  
 
-    sleep(1000)  # Esperar 1 segundo
-
-while True:    # facemos que trabaja siempre
-       
-    if luz < 700:              # Si luz < que 700 es noche 
-        led.write_digital(1)   # Encedemos o LED
-
-    else:                     
-        led.write_digital(0)   # Apagamos o LED
-
-    sleep(1000)
+    sleep(1000)  
