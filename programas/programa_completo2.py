77aafadd3d5b2ca979ce8e6508fbc4b07d2d9a19
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-19 09:28:48 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -5,3 +5,25 @@ Data: 19/05/2025
 from microbit import *
 import music
 import neopixel
+
+#P1 medicion de temperatura
+
+np = neopixel.NeoPixel(pin13, 2)  
+rele = pin16 
+
+np.clear()
+while True:
+    temp = temperature()
+
+    if temp > 20:
+        np[0] = (0, 255, 0)  
+        np[1] = (0, 255, 0)
+        np.show()  
+        rele.write_digital(1)  
+    else:
+        np[0] = (255, 0, 0)  
+        np[1] = (255, 0, 0) 
+        np.show()  
+        rele.write_digital(0)  
+
+    sleep(1000)  
