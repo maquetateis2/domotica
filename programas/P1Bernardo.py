fa50d0dff97e34c376ced1a0d5822b4dddbfbd23
maquetateis2 <maqueta2@iesteis.net>
2025-05-19 09:45:57 +0200

    Update P1Bernardo.py

--- programas/P1Bernardo.py
+++ programas/P1Bernardo.py
@@ -7,7 +7,7 @@ import neopixel
 
 np = neopixel.NeoPixel(pin13, 2)  # 2 LED neopixel conectados ao pin 13
 
-led = pin14 # LED branco conectado ao pin14
+rele = pin16 # LED branco conectado ao pin14
 np.clear()
 while True:
     temperatura = temperature() # gardamos valor da temperatura
@@ -16,12 +16,12 @@ while True:
         np[0] = (0, 255, 0)  # Acender os Neopixel en vermello
         np[1] = (0, 255, 0)
         np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(1)  # Acender o LED normal
+        rele.write_digital(0)  # Acender o LED normal
     else:
         np[0] = (255, 0, 0)  # Apagar os Neopixel
         np[1] = (255, 0, 0)  # Acender os Neopixel en verde
         np.show()  # Mostrar a cor nos neopixel
-        led.write_digital(0)  # Apagar o LED normal
+        rele.write_digital(1)  # Apagar o LED normal
 
     sleep(1000)  # Esperar 1 segundo
 
