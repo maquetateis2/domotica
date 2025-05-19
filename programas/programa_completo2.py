97c6aced8e267db20ba072e0da5c1feb6b5e5f25
leylafp17 <fernandezpazoleyla@gmail.com>
2025-05-19 09:22:24 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -47,6 +47,18 @@ while True:
 
     sleep(1000)  
 
+led = pin14 # conectamos a pin
+
+while True: # facemos que trabaja siempre
+    luz = pin1.read_analog() # leemos el balor
+
+    if luz < 700:            # si luz < que 700 es noche 
+        led.write_digital(1) # y Led encendemos
+
+    else:                    # en otro caso 
+        led.write_digital(0) # lo apagamos
+
+    sleep(100)
 
 
 
