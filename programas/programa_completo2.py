f3d66cae563143a930bc9b7d377ec08a8509ba15
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-19 09:35:47 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -45,9 +45,6 @@ while True: # facemos que trabaja siempre
     sleep(100)
 
 #P3  Programa que simula un timbre dunha casa
-from microbit import *
-import music
-
 led = pin14
 c = 0
 
@@ -65,4 +62,20 @@ while True:
         sleep(1000)
 
 
+#Programa 4 da fila 2 para a maqueta domótica. 
+pin2.set_analog_period(20) # Servo coenctado al pin 2
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
+ 
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
+
+    sleep(100)
+       
 
