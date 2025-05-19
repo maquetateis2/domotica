4492ce38a919b1b84c7bae21e867ea825e8b946a
Isabelly-s <isabellydasilva@iesteis.net>
2025-05-19 09:47:56 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -1,17 +1,24 @@
 """Programa completo da fila 1 para a maqueta domótica. 
-Autores: Isabelly, Sara Aguado, Leyla y Tresandí
+Autores: Isabelly, Sasha, Sara Aguado, Leyla y Tresandí
 Data: 16/05/2025
 """
-""" Práctica 1 Python Micro:bit. LED Neopixel
-Autor: Maria Isabelly Da Silva Cavalcante
-Data:05/05/2025
-"""
+#Programa 1 
+
+ # importamos librerias 
 from microbit import *
 import neopixel
+import music
+
 
 np = neopixel.NeoPixel(pin13, 2)  
-
 rele = pin16 
+led = pin14 # conectamos a pin
+c = 0
+pin2.set_analog_period(20) # Servo coenctado al pin 2
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
+ 
+
 np.clear()
 while True:
     temp = temperature()
@@ -28,15 +35,9 @@ while True:
         rele.write_digital(0)  
 
     sleep(1000)  
-
-"""Programa 2: Control de luz en la casa
-Autor: Dementiev Oleksasndr
-Data: 30/04/2025"""
-
-from microbit import * # importamos libreria
-
-led = pin14 # conectamos a pin
-
+    
+  
+#Programa 2: Control de luz en la casa
 while True: # facemos que trabaja siempre
     luz = pin1.read_analog() # leemos el balor
 
@@ -48,4 +49,62 @@ while True: # facemos que trabaja siempre
 
     sleep(100)
 
+# Programa3: que simula un timbre dunha casa
+while True:
+    if button_a.is_pressed():
+        for c in range(3):
+            c = c+1
+            led.write_digital(1)
+            sleep(500)
+            led.write_digital(0)
+            sleep(500)
 
+    for c in range(2):
+        music.play(music.RINGTONE)
+        sleep(1000)
+
+#
+ 
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
+
+    sleep(100)
+
+
+#
+while True:                           # Bucle infinito
+    sensor = pin15.read_digital()       # Define o pin 15 como "sensor"
+    if sensor == 1:                   # Se o sensor PÎR detecta movemento
+        music.play(music.RINGTONE)       # Reproduce un ton de llamada
+        sleep(500)     
+        music.play(music.RINGTONE)
+        
+        for i in range(5):               # Repite 5 veceso seguinte bloque
+            display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit  
+            np[0] = (0, 255, 0)          # Acende o LED NeoPixel en vermello
+            np[1] = (0, 255, 0)
+            np.show()                    # Mostra o cambio do color no LED
+            led.write_digital(1)         # Acende o LED branco conectadoao pin 14
+            sleep(500)                   # Espera 500 milisegundos
+            np[0] = (0, 0, 0)            # Apaga o LED NeoPixel
+            np[1] = (0, 0, 0)
+            np.show()                    # Actualiza o estado do LED
+            led.write_digital(0)         # Apaga o LED branco
+            display.clear()              # Borra o que se mostra na pantalla
+            sleep(500)
+        
+    else:                             # Se non detecta movemento
+        display.show(Image.HOUSE)     # Mostra unha imaxe dunha casa na pantalla
+    sleep(100) 
+      
+  
+
+
+    
+       
