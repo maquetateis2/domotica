2420d7aff0800d4a5c9a9645df8b49dfc77cfbca
leylafp17 <fernandezpazoleyla@gmail.com>
2025-05-19 09:23:50 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -77,4 +77,18 @@ while True:
     for c in range(2):
         music.play(music.RINGTONE)
         sleep(1000)
+pin2.set_analog_period(20) # Servo coenctado al pin 2
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
+ 
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
 
+    sleep(100)
+       
