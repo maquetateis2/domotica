7b592286299d9c1d481f7d81ecbff9fa1fa160fc
leylafp17 <fernandezpazoleyla@gmail.com>
2025-05-19 09:21:01 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -25,8 +25,27 @@ while True:
 
     sleep(1000)  # Esperar 1 segundo
 
+import neopixel
 
+np = neopixel.NeoPixel(pin13, 2)  
 
+rele = pin16 
+np.clear()
+while True:
+    temp = temperature()
+
+    if temp > 20:
+        np[0] = (0, 255, 0)  
+        np[1] = (0, 255, 0)
+        np.show()  
+        rele.write_digital(1)  
+    else:
+        np[0] = (255, 0, 0)  
+        np[1] = (255, 0, 0) 
+        np.show()  
+        rele.write_digital(0)  
+
+    sleep(1000)  
 
 
 
