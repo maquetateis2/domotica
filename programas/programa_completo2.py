756d303bea7da8fc1867aea61b19ab172017b2e1
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-19 09:39:42 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -6,7 +6,7 @@ from microbit import *
 import music
 import neopixel
 
-#P1 medicion de temperatura
+#P1 Medicion de temperatura
 
 np = neopixel.NeoPixel(pin13, 2)  
 rele = pin16 
@@ -62,7 +62,7 @@ while True:
         sleep(1000)
 
 
-#Programa 4 da fila 2 para a maqueta domótica. 
+#P4 iyefgeirhfiurehgfu4ehj
 pin2.set_analog_period(20) # Servo coenctado al pin 2
 pin2.write_analog(1) #La puerta comienza cerrada
 porta = 0 
@@ -77,5 +77,38 @@ while True :
             porta=0
 
     sleep(100)
-       
+
+
+
+#Programa 5 da fila 2 para a maqueta domótica. 
+
+np = neopixel.NeoPixel(pin13, 2)    # Crea unha tira de 1 LED NeoPixel conectado ao pin 13
+led = pin14                         # Define o pin 14 como "led"
+
+
+while True:                           # Bucle infinito
+    sensor = pin15.read_digital()       # Define o pin 15 como "sensor"
+    if sensor == 1:                   # Se o sensor PÎR detecta movemento
+        music.play(music.RINGTONE)       # Reproduce un ton de llamada
+        sleep(500)     
+        music.play(music.RINGTONE)
+        
+        for i in range(5):               # Repite 5 veceso seguinte bloque
+            display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit  
+            np[0] = (0, 255, 0)          # Acende o LED NeoPixel en vermello
+            np[1] = (0, 255, 0)
+            np.show()                    # Mostra o cambio do color no LED
+            led.write_digital(1)         # Acende o LED branco conectadoao pin 14
+            sleep(500)                   # Espera 500 milisegundos
+            np[0] = (0, 0, 0)            # Apaga o LED NeoPixel
+            np[1] = (0, 0, 0)
+            np.show()                    # Actualiza o estado do LED
+            led.write_digital(0)         # Apaga o LED branco
+            display.clear()              # Borra o que se mostra na pantalla
+            sleep(500)
+        
+    else:                             # Se non detecta movemento
+        display.show(Image.HOUSE)     # Mostra unha imaxe dunha casa na pantalla
+    sleep(100) 
+      
 
