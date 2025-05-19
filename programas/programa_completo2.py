086973ee9f8f01174baa7ce86756992276407a86
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-19 09:33:46 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -43,3 +43,23 @@ while True: # facemos que trabaja siempre
         led.write_digital(0) # lo apagamos
 
     sleep(100)
+
+#P3 
+from microbit import *
+import music
+
+led = pin14
+c = 0
+
+while True:
+    if button_a.is_pressed():
+        for c in range(3):
+            c = c+1
+            led.write_digital(1)
+            sleep(500)
+            led.write_digital(0)
+            sleep(500)
+
+    for c in range(2):
+        music.play(music.RINGTONE)
+        sleep(1000)
