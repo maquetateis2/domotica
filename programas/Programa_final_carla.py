ba317ba380533c9bf19f2c542e76af252aa44fde
Carla Tresandí <tresandic@gmail.com>
2025-05-20 13:58:44 +0200

    Update Programa_final_carla.py

--- programas/Programa_final_carla.py
+++ programas/Programa_final_carla.py
@@ -8,9 +8,10 @@ import music
 
 np = neopixel.NeoPixel(pin13, 2)  
 
+pin2.write_analog(1) #La puerta comienza cerrada
+porta = 0 
 led = pin14 # conectamos a pin
 rele = pin16 
-np.clear()
 c = 0
 
 while True:
@@ -52,3 +53,16 @@ while True:
     for c in range(2):
         music.play(music.RINGTONE)
         sleep(1000)
+        pin2.set_analog_period(20) # Servo coenctado al pin 2
+
+while True :
+    if button_b.is_pressed(): #Si el botón b esta presionado
+        if porta == 0:
+            pin2.write_analog(90) #Puerta abierta a 90º
+            porta=1
+        else:
+            pin2.write_analog(1) 
+            porta=0
+
+    sleep(100)
+       
