8d589b7aacf2823352df5e4aaebda2b099403091
Sasha-teis6 <oleksandrdementiev@iesteis.net>
2025-05-20 14:15:29 +0200

    Update programa_completo2.py

--- programas/programa_completo2.py
+++ programas/programa_completo2.py
@@ -1,115 +1,89 @@
 """Programa completo da fila 1 para a maqueta domótica. 
-Autores: Sasha,Isabely,Leyla,Sara,Carla
+Autores: Sasha, Isabely, Leyla, Sara, Carla
 Data: 19/05/2025
 """
-from microbit import *
-import music
-import neopixel
 
+from microbit import *      # Importamos todas as funcións necesarias da placa Micro:bit
+import music                # Importamos o módulo para melodías
+import neopixel             # Importamos o módulo para NeoPixel
 
-#P1 Medicion de temperatura
+rele = pin16                # O relé está conectado ao pin 16
+led = pin14                 # Un LED está conectado ao pin 14
+pin2.set_analog_period(20)  # O pin 2 para un servo motor
+np = neopixel.NeoPixel(pin13, 2)  # NeoPixel conectados ao pin 13
 
-np = neopixel.NeoPixel(pin13, 2)  
-rele = pin16 
+porta = 0                   # Variable para gardar o estado da porta
+pin2.write_analog(1)        # porta pechada
+np.clear()                  # Apagamos todos os LEDs NeoPixel ao comezo
 
-np.clear()
 while True:
-    temp = temperature()
 
+# --- P1: Medición de temperatura ---
+    temp = temperature()   # Lemos a temperatura
+    
     if temp > 20:
-        np[0] = (0, 255, 0)  
+        np[0] = (0, 255, 0)    # Verde nos LEDs NeoPixel
         np[1] = (0, 255, 0)
-        np.show()  
-        rele.write_digital(0)  
+        np.show()             # Actualizamos os LEDs
+        rele.write_digital(0) # Apagamos o relé
     else:
-        np[0] = (255, 0, 0)  
-        np[1] = (255, 0, 0) 
-        np.show()  
-        rele.write_digital(1)  
+        np[0] = (255, 0, 0)    # Vermello nos LEDs NeoPixel
+        np[1] = (255, 0, 0)
+        np.show()             # Actualizamos os LEDs
+        rele.write_digital(1) # Acendemos o relé 
 
-    sleep(1000)  
+# --- P2: Control de luz ambiente ---
+    luz = pin1.read_analog()  # Lemos a luz (sensor LDR)
+    
+    if luz < 700:             # Se hai pouca luz (de noite)
+        led.write_digital(1)  # Encendemos o LED
+    else:                     # Se hai luz suficiente (de día)
+        led.write_digital(0)  # Apagamos o LED branco
 
-
-#P2 Control de luz
-
-led = pin14 # conectamos a pin
-
-while True: # facemos que trabaja siempre
-    luz = pin1.read_analog() # leemos el balor
-
-    if luz < 700:            # si luz < que 700 es noche 
-        led.write_digital(1) # y Led encendemos
-
-    else:                    # en otro caso 
-        led.write_digital(0) # lo apagamos
-
-    sleep(100)
-
-#P3  Programa que simula un timbre dunha casa
-led = pin14
-c = 0
-
-while True:
-    if button_a.is_pressed():
-        for c in range(3):
-            c = c+1
+# --- P3: Timbre da casa ---
+    if button_a.is_pressed():        # Se se preme o botón A
+        for c in range(3):           # Parpadea o LED 3 veces
             led.write_digital(1)
             sleep(500)
             led.write_digital(0)
             sleep(500)
 
-    for c in range(2):
-        music.play(music.RINGTONE)
-        sleep(1000)
+        for c in range(2):           # Reproduce o timbre 2 veces
+            music.play(music.RINGTONE)
+            sleep(1000)
 
+# --- P4: Control da porta con servo ---
+    if button_b.is_pressed():        # Se se preme o botón B
+        if porta == 0:               # Se a porta está pechada
+            pin2.write_analog(90)    # Abrímosla
+            porta = 1                # Actualizamos o estado
+        else:                        # Se xa está aberta
+            pin2.write_analog(1)     # Pechámosla
+            porta = 0                # Actualizamos o estado
+            sleep(500)               # Esperamos un pouco para evitar rebotes do botón
 
-#P4 iyefgeirhfiurehgfu4ehj
-pin2.set_analog_period(20) # Servo coenctado al pin 2
-pin2.write_analog(1) #La puerta comienza cerrada
-porta = 0 
- 
-while True :
-    if button_b.is_pressed(): #Si el botón b esta presionado
-        if porta == 0:
-            pin2.write_analog(90) #Puerta abierta a 90º
-            porta=1
-        else:
-            pin2.write_analog(1) 
-            porta=0
-
-    sleep(100)
-
-
-
-#Programa 5 da fila 2 para a maqueta domótica. 
-
-np = neopixel.NeoPixel(pin13, 2)    # Crea unha tira de 1 LED NeoPixel conectado ao pin 13
-led = pin14                         # Define o pin 14 como "led"
-
-
-while True:                           # Bucle infinito
-    sensor = pin15.read_digital()       # Define o pin 15 como "sensor"
-    if sensor == 1:                   # Se o sensor PÎR detecta movemento
-        music.play(music.RINGTONE)       # Reproduce un ton de llamada
-        sleep(500)     
+# --- P5: Alarma de movemento con sensor PIR ---
+    sensor = pin15.read_digital()     # Lemos o estado do sensor (PIR)
+    
+    if sensor == 1:                   # Se detecta movemento
+        music.play(music.RINGTONE)    # Reproducimos un son de alarma
+        sleep(500)
         music.play(music.RINGTONE)
         
-        for i in range(5):               # Repite 5 veceso seguinte bloque
-            display.show(Image.ANGRY)    # Mostra unha cara enfadada na pantalla da microbit  
-            np[0] = (0, 255, 0)          # Acende o LED NeoPixel en vermello
+        for i in range(5):            # Repetimos a alarma 5 veces
+            display.show(Image.ANGRY) # Amosamos cara enfadada na pantalla
+            np[0] = (0, 255, 0)       # Encendemos os NeoPixel en verde
             np[1] = (0, 255, 0)
-            np.show()                    # Mostra o cambio do color no LED
-            led.write_digital(1)         # Acende o LED branco conectadoao pin 14
-            sleep(500)                   # Espera 500 milisegundos
-            np[0] = (0, 0, 0)            # Apaga o LED NeoPixel
-            np[1] = (0, 0, 0)
-            np.show()                    # Actualiza o estado do LED
-            led.write_digital(0)         # Apaga o LED branco
-            display.clear()              # Borra o que se mostra na pantalla
+            np.show()
+            led.write_digital(1)      # Encendemos o LED branco
             sleep(500)
-        
-    else:                             # Se non detecta movemento
-        display.show(Image.HOUSE)     # Mostra unha imaxe dunha casa na pantalla
-    sleep(100) 
-      
+            np[0] = (0, 0, 0)         # Apagamos os NeoPixel
+            np[1] = (0, 0, 0)
+            np.show()
+            led.write_digital(0)      # Apagamos o LED branco
+            display.clear()           # Borramos a pantalla
+            sleep(500)
+    else:
+        display.show(Image.HOUSE)     # Se non hai movemento, amosamos unha casa tranquila
 
+    sleep(100)  # Pequena pausa para evitar que o bucle sexa demasiado rápido
